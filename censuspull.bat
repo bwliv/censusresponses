f9ffe898797d1ff800cff3e94e9c36568e17ed95
Benjamin Livingston <bwliving@gmail.com>
2020-03-21 14:09:06 -0400

    folders reorganized / new covid-19 research

--- censuspull.bat
+++ censuspull.bat
@@ -1,2 +1,2 @@
-cd C:\Users\bwliv\OneDrive\Documents\censusresponses\daily_datasets
-python C:\Users\bwliv\OneDrive\Documents\censusresponses\pull2020_full_dataset.py
+cd C:\Users\bwliv\OneDrive\Documents\censusresponses\2020_data
+python C:\Users\bwliv\OneDrive\Documents\censusresponses\2020_data\pull2020_full_dataset.py
